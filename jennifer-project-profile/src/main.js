26f13b2f1f9c6937413a6453d4892cf76b648035
Jennifer Pichardo <jenniferpichardo15@gmail.com>
2024-05-30 14:01:58 -0400

    Added project profile page

---
+++ jennifer-project-profile/src/main.js
@@ -0,0 +1,63 @@
+// Jennifer Pichardo
+// Project Profile Page
+
+document.addEventListener("DOMContentLoaded", function() { 
+    const carousel = document.querySelector(".carousel"); 
+    const arrowBtns = document.querySelectorAll(".wrapper i"); 
+    const wrapper = document.querySelector(".wrapper"); 
+  
+    const firstCard = carousel.querySelector(".card"); 
+    const firstCardWidth = firstCard.offsetWidth; 
+  
+    let isDragging = false, 
+        startX, 
+        startScrollLeft, 
+        timeoutId; 
+  
+    const dragStart = (e) => {  
+        isDragging = true; 
+        carousel.classList.add("dragging"); 
+        startX = e.pageX; 
+        startScrollLeft = carousel.scrollLeft; 
+    }; 
+  
+    const dragging = (e) => { 
+        if (!isDragging) return; 
+      
+        // Calculate the new scroll position 
+        const newScrollLeft = startScrollLeft - (e.pageX - startX); 
+      
+        // Check if the new scroll position exceeds  
+        // the carousel boundaries 
+        if (newScrollLeft <= 0 || newScrollLeft >=  
+            carousel.scrollWidth - carousel.offsetWidth) { 
+              
+            // If so, prevent further dragging 
+            isDragging = false; 
+            return; 
+        } 
+      
+        // Otherwise, update the scroll position of the carousel 
+        carousel.scrollLeft = newScrollLeft; 
+    }; 
+  
+    const dragStop = () => { 
+        isDragging = false;  
+        carousel.classList.remove("dragging"); 
+    }; 
+  
+    carousel.addEventListener("mousedown", dragStart); 
+    carousel.addEventListener("mousemove", dragging); 
+    document.addEventListener("mouseup", dragStop); 
+    wrapper.addEventListener("mouseenter", () =>  
+        clearTimeout(timeoutId)); 
+  
+    // Add event listeners for the arrow buttons to  
+    // scroll the carousel left and right 
+    arrowBtns.forEach(btn => { 
+        btn.addEventListener("click", () => { 
+            carousel.scrollLeft += btn.id === "left" ?  
+                -firstCardWidth : firstCardWidth; 
+        }); 
+    }); 
+}); 

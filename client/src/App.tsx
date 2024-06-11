0486665c8e842b17527af587edfb553d9b3768df
Joseph Dunne <54995769+joedunne-rit@users.noreply.github.com>
2024-06-11 11:05:46 -0400

    Side bar branch

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,10 +3,12 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import * as paths from "./constants/routes";
 import Home from "./components/pages/Home";
 import NotFound from "./components/pages/NotFound";
+import SideBar from "./components/Sidebar";
 
 function App() {
   return (
     <div className="App">
+      <SideBar />
       <BrowserRouter>
         <Routes>
           <Route path={paths.routes.DEFAULT} element={<Home />} />

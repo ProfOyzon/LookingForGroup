b54c0b7f739c9788de247eaa09e1f857c79bc4a5
Abraham Furlan <abrahamfurlan@gmail.com>
2024-06-24 11:00:30 -0400

    implemented account database functions

--- server/controllers/Database.js
+++ server/controllers/Database.js
@@ -13,23 +13,137 @@ const notFound = (req, res) => {
 // Accounts
 
 const login = (req, res) => {
-    return res.json({ message: "Not Implemented Yet" });
+    const username = `${req.body.username}`;
+    const password = `${req.body.password}`;
+
+    // make sure that we recieved credentials
+    if (!username || !password) {
+        return res.status(400).json({ error: 'Missing Credentials' });
+    }
+
+    // use the authenticate function in the Account model to login to an account
+    return Account.authenticate(username, password, (err, account) => {
+        if (err || !account) {
+            return res.status(401).json({ error: 'Login Failed' });
+        }
+
+        // update the current session to use this account (and convert it to usable json)
+        req.session.account = Account.toAPI(account);
+
+
+        return res.json({
+            loggedIn: true,
+            account: req.session.account,
+            redirect: '/'
+        });
+    });
 }
 
-const signup = (req, res) => {
-    return res.json({ message: "Not Implemented Yet" });
+const signup = async (req, res) => {
+    const email = `${req.body.email}`;
+    const username = `${req.body.username}`;
+    const password1 = `${req.body.password}`;
+    const password2 = `${req.body.password2}`;
+
+    // make sure that we recieved credentials
+    if (!username || !password1 || !password2 || !email) {
+        return res.status(400).json({ error: 'Missing Credentials' });
+    }
+
+    // make sure that passwords match
+    if (password1 !== password2) {
+        return res.status(400).json({ error: 'Passwords do not match' });
+    }
+
+    // make sure that email is in the correct format
+    if (!/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email)) {
+        return res.status(400).json({ error: 'Email does not match pattern' });
+    }
+
+    // make sure that the username is in the correct format
+    if (!/^[A-Za-z0-9_\-.]{1,16}$/.test(username)) {
+        return res.status(400).json({ error: 'Username does not match pattern' });
+    }
+
+    // makes sure that the password is in the correct format
+    if (!/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(password1)) {
+        return res.status(400).json({ error: 'Password does not match pattern' });
+    }
+
+    try {
+        // create a placeholder profile picture
+        const profilePicture = {
+            name: "default-profile",
+            data: null,
+            mimeType: null,
+        }
+
+        // use the generateHash function in the Account model to encrypt the password
+        const hash = await Account.generateHash(password1);
+
+        // create a new account
+        const newAccount = new Account({ email, username, password: hash, profilePicture });
+        await newAccount.save();
+
+        // update the current session to use this account (and convert it to usable json)
+        req.session.account = Account.toAPI(newAccount);
+
+        return res.json({
+            loggedIn: true,
+            account: req.session.account
+        });
+    } catch (err) {
+        console.log(err);
+        if (err.code === 11000) { // this error code means that a unique value already exists
+            return res.status(400).json({ error: 'Username already in use' });
+        }
+        return res.status(400).json({ error: 'An error occurred' });
+    }
 }
 
-const updateAccountByID = (req, res) => {
-    return res.json({ message: "Not Implemented Yet" });
+const logout = (req, res) => {
+    // destroys the current session
+    req.session.destroy();
+
+    // redirects to the default home page
+    return res.json({
+        loggedIn: false,
+        redirect: '/'
+    });
+};
+
+const updateAccountByID = async (req, res) => {
+    try {
+        // This data object represents any changes we are making to the Account
+        data = {
+            email: req.body.data.email ? req.body.data.email : null,
+            username: req.body.data.username ? req.body.data.username : null,
+            profilePicture: req.body.data.profilePicture ? req.body.data.profilePicture : null,
+            name: req.body.data.name ? req.body.data.name : null,
+            pronouns: req.body.data.pronouns ? req.body.data.pronouns : null,
+            bio: req.body.data.bio ? req.body.data.bio : null,
+            skills: req.body.data.skills ? req.body.data.skills : null,
+        }
+
+        // find and update the doc
+        const result = await Project.findOneAndUpdate({ _id: req.body.data._id }, data, { new: true, runValidators: true });
+        // push the change to the database
+        result.save();
+        return res.json({ account: result });
+    } catch (err) {
+        console.log(err);
+        return res.status(400).json({ error: 'An error occurred' });
+    }
 }
 
-const getAccountByID = (req, res) => {
-    return res.json({ message: "Not Implemented Yet" });
-}
-
-const addSkillsByAccountID = (req, res) => {
-    return res.json({ message: "Not Implemented Yet" });
+const getAccountByID = async (req, res) => {
+    try {
+        const result = await Project.findById(req.body._id);
+        return res.json({ account: result });
+    } catch (err) {
+        console.log(err);
+        return res.status(400).json({ error: 'An error occurred' });
+    }
 }
 
 
@@ -79,6 +193,7 @@ module.exports = {
     notFound,
     login,
     signup,
+    logout,
     updateAccountByID,
     getAccountByID,
     addSkillsByAccountID,
--- server/router.js
+++ server/router.js
@@ -7,27 +7,28 @@ const router = (app) => {
     app.get("/notFound", mid.requiresSecure, controllers.Database.notFound);
 
     // Accounts
-    app.get("/login", mid.requiresSecure, controllers.Database.login);
-    app.get("/signup", mid.requiresSecure, controllers.Database.signup);
-    app.get("/updateAccountByID", mid.requiresSecure, controllers.Database.updateAccountByID);
+    app.get("/login", mid.requiresSecure, mid.requiresLogout, controllers.Database.login);
+    app.get("/signup", mid.requiresSecure, mid.requiresLogout, controllers.Database.signup);
+    app.get("/logout", mid.requiresSecure, mid.requiresLogin, controllers.Database.logout);
+    app.get("/updateAccountByID", mid.requiresSecure, mid.requiresLogin, controllers.Database.updateAccountByID);
     app.get("/getAccountByID", mid.requiresSecure, controllers.Database.getAccountByID);
-    app.get("/addSkillsByAccountID", mid.requiresSecure, controllers.Database.addSkillsByAccountID);
+    app.get("/addSkillsByAccountID", mid.requiresSecure, mid.requiresLogin, controllers.Database.addSkillsByAccountID);
 
     // Projects
-    app.get("/createProject", mid.requiresSecure, controllers.Database.createProject);
-    app.get("/updateProjectByID", mid.requiresSecure, controllers.Database.updateProjectByID);
+    app.get("/createProject", mid.requiresSecure, mid.requiresLogin, controllers.Database.createProject);
+    app.get("/updateProjectByID", mid.requiresSecure, mid.requiresLogin, controllers.Database.updateProjectByID);
 
     // Posts
-    app.get("/createPost", mid.requiresSecure, controllers.Database.createPost);
-    app.get("/updatePostByID", mid.requiresSecure, controllers.Database.updatePostByID);
+    app.get("/createPost", mid.requiresSecure, mid.requiresLogin, controllers.Database.createPost);
+    app.get("/updatePostByID", mid.requiresSecure, mid.requiresLogin, controllers.Database.updatePostByID);
 
     // Comments
-    app.get("/createComment", mid.requiresSecure, controllers.Database.createComment);
-    app.get("/updateCommentByID", mid.requiresSecure, controllers.Database.updateCommentByID);
+    app.get("/createComment", mid.requiresSecure, mid.requiresLogin, controllers.Database.createComment);
+    app.get("/updateCommentByID", mid.requiresSecure, mid.requiresLogin, controllers.Database.updateCommentByID);
 
     // Messages 
-    app.get("/createMessage", mid.requiresSecure, controllers.Database.createMessage);
-    app.get("/updateMessageByID", mid.requiresSecure, controllers.Database.updateMessageByID);
+    app.get("/createMessage", mid.requiresSecure, mid.requiresLogin, controllers.Database.createMessage);
+    app.get("/updateMessageByID", mid.requiresSecure, mid.requiresLogin, controllers.Database.updateMessageByID);
 
     // Testing 
     app.get("/TestAccountSchema", mid.requiresSecure, controllers.Tests.makeFakeAccount);

34bb1bce16ef41063346495d325b04c8094e5a41
Joseph Dunne <54995769+joedunne-rit@users.noreply.github.com>
2024-07-01 16:02:36 -0400

    Project page navigation update
    Added extra navigation to project page. Now, clicking on members or project posts will redirect to the profile page or project post page, respectively.

--- client/src/components/projectPageComponents/ProjectPost.tsx
+++ client/src/components/projectPageComponents/ProjectPost.tsx
@@ -1,9 +1,12 @@
 import "../styles.css"
 import postImagePlaceholder from "../../img/AhomeIcon.png";
+import { useNavigate } from 'react-router-dom';
+import * as paths from "../../constants/routes";
 
 export const ProjectPost = (props) => {
+  const navigate = useNavigate();
   return (
-    <div className='project-post'>
+    <div className='project-post' onClick={() => navigate(paths.routes.PROJECTPOST)}>
       <img src={postImagePlaceholder} alt=''/>
       <div className='post-content'>
         <div className='post-info'>

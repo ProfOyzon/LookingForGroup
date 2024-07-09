9cb58c4758e0829baadc681fe72ca3703006c90e
mivansek <92755769+mivansek@users.noreply.github.com>
2024-07-09 11:09:16 -0500

    Added search bar to the messages page

--- client/src/components/MessagesDisplay.tsx
+++ client/src/components/MessagesDisplay.tsx
@@ -1,12 +1,23 @@
 import React from "react";
 import { MessageCard } from "./MessageCard";
 import { messages } from "../constants/fakeData";
+import { SearchBar } from "./SearchBar";
+import { useState, useCallback } from "react";
 
 export const MessagesDisplay = ({userID}) => {
+
+    // --- Searching ---
+    const [filteredMessages, setFilteredMessages] = useState(messages);
+
+    const HandleSearch = useCallback((results) => {
+        setFilteredMessages(results[0]);
+      }, []);
+
     return(
         <div>
+            <SearchBar dataSets={[{ data: messages }]} onSearch={HandleSearch}></SearchBar>
             {
-                messages.map(msg => {
+                filteredMessages.map(msg => {
                     if(msg.recipientID == userID){
                         return (
                             <MessageCard msg={msg} />

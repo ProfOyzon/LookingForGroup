6fc85138e1461c2855979f0bf005bcd9d1e268f6
svilinskis1 <svilinskis1@gmail.com>
2024-08-07 14:01:13 -0400

    merge

--- client/src/components/Profile/ProfilePreferences.tsx
+++ client/src/components/Profile/ProfilePreferences.tsx
@@ -9,13 +9,9 @@ export const ProfilePreferences = ({ user }) => {
   // const [showInterestsModal, setShowInterestsModal] = useState(false);
   // const [selectedInterests, setSelectedInterests] = useState(user.preferences);
 
-<<<<<<< HEAD
-export const ProfilePreferences = ({user}) => {
   let listContent;
   listContent = user.preferences.map(preference => <Tags>{preference}</Tags>)
 
-=======
->>>>>>> 7a39c1bbb66a6b5461a5db7f83209f7a8f8f8fc5
   const [showPopup, setShowPopup] = useState(false);
   let openPopups = [showPopup];
 
@@ -24,18 +20,18 @@ export const ProfilePreferences = ({user}) => {
   }
 
   return (
-<<<<<<< HEAD
-      <section id = "profile-preferences">
-        <div className="profile-name-button">
-          <h1>Preferences</h1>
-          <button className="icon-button" onClick={() => openClosePopup(showPopup, setShowPopup, openPopups)}><img src = {edit}/></button>
-        </div>
-        <div id = "profile-preferences-list" className="profile-list">
-          {listContent}
-        </div>
+    <section id="profile-preferences">
+      <div className="profile-name-button">
+        <h1>Preferences</h1>
+        <button className="icon-button" onClick={() => openClosePopup(showPopup, setShowPopup, openPopups)}><img src={edit} /></button>
+        {/* <button className="icon-button" onClick={() => setShowInterestsModal(true)}><img src={edit} /></button> */}
+      </div>
+      <div id="profile-preferences-list" className="profile-list">
+        {listContent}
+      </div>
 
         <PagePopup width={'80vw'} height={'80vh'} popupId={0} zIndex={3} show={showPopup} setShow={setShowPopup} openPopups={openPopups}>
-          <div id="profile-edit-prefs" className="profile-edit">
+        <div id="profile-edit-prefs" className="profile-edit">
           <h1>Edit Interests</h1>
               <h3>Select 3 or 4 interests to be highlighted on your page</h3>
               <SearchBar dataSets={[{ }]} onSearch={Search}></SearchBar>
@@ -45,22 +41,6 @@ export const ProfilePreferences = ({user}) => {
                 {listContent}
               </div>
           </div>
-=======
-    <section id="profile-preferences">
-      <div className="profile-name-button">
-        <h1>Preferences</h1>
-        <button className="icon-button" onClick={() => openClosePopup(showPopup, setShowPopup, openPopups)}><img src={edit} /></button>
-        {/* <button className="icon-button" onClick={() => setShowInterestsModal(true)}><img src={edit} /></button> */}
-      </div>
-      <div id="profile-preferences-list" className="profile-list">
-        {user.preferences.map(preference => <Tags>{preference}</Tags>)}
-      </div>
-
-      <PagePopup width={'80vw'} height={'80vh'} popupId={0} zIndex={3} show={showPopup} setShow={setShowPopup} openPopups={openPopups}>
-        <div id='profile-edit-prefs'>
-          <p>edit preferences here</p>
-        </div>
->>>>>>> 7a39c1bbb66a6b5461a5db7f83209f7a8f8f8fc5
       </PagePopup>
 
         {/*  WORK IN PROGRESS 

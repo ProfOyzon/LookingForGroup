a35c3307bb846a9ee14c2ef4536289d551960dcb
mivansek <92755769+mivansek@users.noreply.github.com>
2024-08-07 02:14:30 -0500

    Fixed the message search bar to search through both the names of senders and message content

--- client/src/components/MessagesDisplay.tsx
+++ client/src/components/MessagesDisplay.tsx
@@ -1,29 +1,81 @@
 import React from "react";
 import { MessageCard } from "./MessageCard";
-import { messages } from "../constants/fakeData";
+import { messages, profiles } from "../constants/fakeData";
 import { SearchBar } from "./SearchBar";
 import { useState, useCallback } from "react";
 
-export const MessagesDisplay = ({userID}) => {
+export const MessagesDisplay = ({ userID }) => {
 
     // --- Searching ---
+    let searchList = [];
+
     const [filteredMessages, setFilteredMessages] = useState(messages);
 
+    // takes the search results and filters them into a new array to be properly displayed
     const HandleSearch = useCallback((results) => {
-        setFilteredMessages(results[0]);
-      }, []);
+        let filteredBlocks = [];
+        let isMessage = false;
+        let alreadyFiltered = false;
+        for (let r of results[0]) {
+            for (let m of messages) {
+                if (r == m) {
+                    if(filteredBlocks.includes(r)){
+                        alreadyFiltered = true;
+                    }
+                    if (!alreadyFiltered) {
+                        filteredBlocks.push(r);
+                        isMessage = true;
+                    }
+                }
+            }
+            // if the result is a profile, this finds the associated message and adds that to the array
+            if (!isMessage) {
+                for (let p of profiles) {
+                    if (r == p) {
+                        for (let m of messages) {
+                            if (p._id == m.senderID) {
+                                if(filteredBlocks.includes(m)){
+                                    alreadyFiltered = true;
+                                }
+                                if (!alreadyFiltered) {
+                                    filteredBlocks.push(m);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            alreadyFiltered = false;
+            isMessage = false;
+        }
+        setFilteredMessages(filteredBlocks);
+    }, []);
 
-    return(
+    // adds all the messages received by the user, and all the profiles
+    // the user has recieved messages from into an array to be searched
+    for (let m of messages) {
+        if (m.recipientID == userID) {
+            for (let p of profiles) {
+                if (m.senderID == p._id) {
+                    searchList.push(p);
+                }
+            }
+            searchList.push(m);
+        }
+    }
+
+    return (
         <div>
-            <SearchBar dataSets={[{ data: messages }]} onSearch={HandleSearch}></SearchBar>
+            <SearchBar dataSets={[{ data: searchList }]} onSearch={HandleSearch}></SearchBar>
             {
                 filteredMessages.map(msg => {
-                    if(msg.recipientID == userID){
+                    if (msg.recipientID == userID) {
                         return (
                             <MessageCard msg={msg} />
                         )
                     }
-                })
+                }
+                )
             }
         </div>
     )

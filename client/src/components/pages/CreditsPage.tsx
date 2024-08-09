5c6d66bac600ae08b7b5d01ba2e17a2f58107646
svilinskis1 <svilinskis1@gmail.com>
2024-08-09 16:17:03 -0400

    added credit

--- client/src/components/pages/CreditsPage.tsx
+++ client/src/components/pages/CreditsPage.tsx
@@ -11,7 +11,7 @@ const Credits = (props) => {
     // MORE (PROBABLY) 
 
     // This is just here to create some placeholder text 
-    let tempCredits = [`Abraham Furlan`, `Ben Gomez`, `Jane Doe`, `Jane Doe`, `Jane Doe`, `Jane Doe`, `Jane Doe`, `Jane Doe`, `Jane Doe`, `Jane Doe`];
+    let tempCredits = [`Abraham Furlan`, `Ben Gomez`, `Samera Vilinksis`, `Jane Doe`, `Jane Doe`, `Jane Doe`, `Jane Doe`, `Jane Doe`, `Jane Doe`, `Jane Doe`];
 
     return (
         <div>

f8be089708d4534e81492085d994e5fefe17a049
jf7689 <jamesfeng51@gmail.com>
2024-09-10 13:39:43 -0400

    Add route to get projects by id.

--- server/controllers/projects.js
+++ server/controllers/projects.js
@@ -2,8 +2,23 @@ import pool from "../config/database.js"
 
 const getProjects = async (req, res) => {
     const rows = await pool.query("SELECT * FROM projects");
-    console.log(rows);
-    return res.status(200).json({data: rows})
+    
+    return res.status(200).json({
+        status: 200,
+        data: rows
+    });
 }
 
-export { getProjects };
+const getProjectById = async (req, res) => {
+    const { id } = req.params;
+    const sql = "SELECT * FROM projects WHERE project_id = ?";
+    const values = [id];
+    const rows = await pool.query(sql, values);
+    
+    return res.status(200).json({
+        status: 200,
+        data: rows
+    });
+}
+
+export { getProjects, getProjectById };
--- server/routes/projects.js
+++ server/routes/projects.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { getProjects } from "../controllers/projects.js";
+import { getProjects, getProjectById } from "../controllers/projects.js";
 
 const router = Router();
 
 router.get("/api/projects", getProjects);
+router.get("/api/projects/:id", getProjectById);
 
 export default router;

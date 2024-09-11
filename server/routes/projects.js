c5ef8b651ccc22015e7f60c186d740a16968c584
jf7689 <jamesfeng51@gmail.com>
2024-09-11 15:14:52 -0400

    Add path for updating project data

--- server/routes/projects.js
+++ server/routes/projects.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { getProjects, getProjectById, createProject } from "../controllers/projects.js";
+import { getProjects, getProjectById, createProject, updateProjectById } from "../controllers/projects.js";
 
 const router = Router();
 
 router.get("/api/projects", getProjects);
 router.post("/api/projects", createProject);
 router.get("/api/projects/:id", getProjectById);
+router.put("/api/projects/:id", updateProjectById);
 
 export default router;

530a9fd2f0a1344f9132ec48e68eb1bb52668022
jf7689 <jamesfeng51@gmail.com>
2024-09-23 16:07:25 -0400

    Start delivery of frontend from backend.

--- server/index.js
+++ server/index.js
@@ -1,17 +1,22 @@
 import express from "express";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
 import morgan from "morgan";
 import usersRouter from "./routes/users.js";
 import projectsRouter from "./routes/projects.js";
 
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = process.env.PORT || process.env.NODE_PORT || 3001;
 
+app.use(express.static(join(__dirname, "../client")));
 app.use(morgan("tiny"));
 app.use(express.json());
 app.use(usersRouter);
 app.use(projectsRouter);
 
-app.get("/", (req, res) => {
+app.get("/api", (req, res) => {
     return res.json({ message: "You Reached The Looking For Group API" });
 })
 

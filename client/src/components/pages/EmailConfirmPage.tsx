bd3c86927e84fd40ad2c37e7048f4ad608710f97
Trevor Dunn <tjd2695@rit.edu>
2024-12-03 13:41:19 -0500

    Email verify mostly working

--- client/src/components/pages/EmailConfirmPage.tsx
+++ client/src/components/pages/EmailConfirmPage.tsx
@@ -1,15 +1,25 @@
 import "./pages.css";
 import "../Styles/styles.css";
 import { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 import * as paths from "../../constants/routes";
+import * as fetch from "../../functions/fetch";
 
 const EmailConfirmation = (props) => {
     // THINGS TO DO:
-    // Add Page Components (info box stating email is confirmed)
+    // Add Page Components (info box stating email is confirmed) x
     // Style page to match Figma (both light and dark modes)
-    // Have countdown to redirect to discover page (as well as link to discover page if redirect doesn't work)
-    // Include code in link as query parameter and work with database to verify users email
+    // Auto redirect to discover page (as well as link to discover page if redirect doesn't work) x
+    // Include token in link as (react search parameter or query parameter) and work with database to verify users email (grab token and fetch to /api/signup/[insert token here]) x
+
+    //Gets current location
+    const location = useLocation();
+    //Gets pathname from current location
+    const path = location.pathname;
+
+    //Retrieves the token from the path using substring
+    //Sends a get request to server to "/api/signup/[token]"
+    fetch.sendGet(`/api/signup/'${path.substring(path.lastIndexOf("/")+1, path.length)}`);
 
     // Hook for navigation
     const navigate = useNavigate();
@@ -26,11 +36,13 @@ const EmailConfirmation = (props) => {
     });
 
     return (
-        <div id="email-confirmation-page">
-            <div id="confirmation-text-box">
-                Your email has been confirmed!
-                You will be automatically redirected soon
-                If you are not automatically redirected, click <span id="manual-redirect-link" onClick={() => redirect()}>here</span>
+        <div id="email-confirmation-page" className="background-cover">
+            <div className="confirmation-container">
+                <div className="text-container">
+                    <h1 id="confirmation-text">Your email has been confirmed!</h1><br></br>
+                    <h2 id="auto-redirect-text">You will be automatically redirected soon</h2><br></br>
+                    <h3 id="manual-redirect-text">If you are not automatically redirected, click <span id="manual-redirect-link" onClick={() => redirect()}><i>here</i></span></h3>
+                </div>
             </div>
         </div>
     );
--- client/src/constants/routes.ts
+++ client/src/constants/routes.ts
@@ -17,5 +17,5 @@ export const routes = {
     "SETTINGS": "/settings",
     "CREDITS": "/credits",
     "NOTFOUND": "/notFound",
-    "EMAILCONFIRM": "/emailConfirmation",
+    "EMAILVERIFY": "/emailVerify/",
 }

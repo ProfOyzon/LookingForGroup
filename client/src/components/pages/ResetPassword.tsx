628ed66eb12c8f7ac830103621b35a1ea00b63d6
Nick Lang <nll5171@rit.edu>
2025-01-17 16:55:15 -0500

    Added password validation to ResetPassword component as well.

--- client/src/components/pages/ResetPassword.tsx
+++ client/src/components/pages/ResetPassword.tsx
@@ -3,15 +3,17 @@ import React, { useState, useEffect } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 import * as paths from "../../constants/routes";
 import { handleError, sendPost, sendGet, hideError } from "../../functions/fetch.js";
+import passwordValidator from 'password-validator';
 
 const ResetPassword = ({ }) => {
     const navigate = useNavigate(); // Hook for navigation
-    const location = useLocation(); 
+    const location = useLocation();
 
     // State variables
     const [passwordInput, setPasswordInput] = useState('');
     const [confirmInput, setConfirmInput] = useState('');
     const [error, setError] = useState(''); // Error message for missing or incorrect information
+    const [passwordMsg, setPasswordMsg] = useState('');
 
     // Function to handle the Set button click
     const handleResetPassword = async () => {
@@ -22,8 +24,13 @@ const ResetPassword = ({ }) => {
             return;
         }
 
+        // Check if the password meets all requirements
+        else if (passwordMsg !== '') {
+            setError('Password must meet all requirements');
+        }
+
         // Check if the passwords match
-        else if(passwordInput !== confirmInput){
+        else if (passwordInput !== confirmInput) {
             setError('The passwords do not match');
             return;
         }
@@ -32,7 +39,7 @@ const ResetPassword = ({ }) => {
             setError('Updating password...');
             // Login for the user
             const path = location.pathname;
-            const token = path.substring(path.lastIndexOf("/")+1, path.length);
+            const token = path.substring(path.lastIndexOf("/") + 1, path.length);
             const data = { password: passwordInput, confirm: confirmInput };
             console.log(data);
             const response = await sendPost(`/api/resets/password/${token}`, data);
@@ -47,6 +54,39 @@ const ResetPassword = ({ }) => {
             // Navigate to the home page
             // navigate(paths.routes.HOME);
         }
+    };
+
+    const validatePassword = (pass) => {
+        // Don't check password if there's nothing there
+        if (pass === '') {
+            return '';
+        }
+
+        const schema = new passwordValidator();
+        schema
+            .is().min(8, 'be 8 or more characters')
+            .is().max(20, 'be 20 or less characters')
+            .has().uppercase(1, 'have an uppercase letter')
+            .has().lowercase(1, 'have a lowercase letter')
+            .has().digits(1, 'have a number')
+            .has().symbols(1, 'have a symbol')
+            .has().not().spaces(1, 'have no spaces')
+            .has().not('[^\x00-\x7F]+', 'have no non-ASCII characters');
+
+        const output = schema.validate(pass, { details: true });
+        let passMsg = '';
+
+        if (output.length > 0) {
+            passMsg += `Password must `;
+
+            for (let i = 0; i < output.length - 1; i++) {
+                passMsg += `${output[i].message}, `;
+            }
+            passMsg += `${(output.length > 1) ? 'and ' : ''}${output[output.length - 1].message}.`;
+        }
+
+        console.log(passMsg);
+        return passMsg;
     };
 
     // Function to handle the forgot pass button click
@@ -68,7 +108,12 @@ const ResetPassword = ({ }) => {
                         type="password"
                         placeholder="New password"
                         value={passwordInput}
-                        onChange={(e) => setPasswordInput(e.target.value)}
+                        onChange={(e) => {
+                            setPasswordInput(e.target.value);
+                            let passMsg = validatePassword(e.target.value);
+                            setError(passMsg);
+                            setPasswordMsg(passMsg);
+                        }}
                     />
                     <input
                         className="login-input"

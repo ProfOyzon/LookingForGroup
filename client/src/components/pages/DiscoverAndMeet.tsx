e2586a8faf82ff6069d11b94a82487e69996808e
Nick Lang <nll5171@rit.edu>
2025-01-23 10:24:10 -0500

    Tags are now correct color, and react to select/unselect.

--- client/src/components/pages/DiscoverAndMeet.tsx
+++ client/src/components/pages/DiscoverAndMeet.tsx
@@ -196,9 +196,9 @@ const DiscoverAndMeet = ({ category }) => {
   let filterPopupTabs =
     category === 'projects'
       ? [
-          { categoryTags: tags.projectTypes, categoryName: 'Project Type' },
-          { categoryTags: tags.genres, categoryName: 'Genre' },
-          { categoryTags: tags.purposes, categoryName: 'Purpose' },
+          { categoryTags: tags.projectTypes, categoryName: 'Project Type', color: 'blue' },
+          { categoryTags: tags.genres, categoryName: 'Genre', color: 'green' },
+          { categoryTags: tags.purposes, categoryName: 'Purpose', color: 'grey' },
         ]
       : [
           { categoryTags: tags.devSkills, categoryName: 'Developer Skill' },
@@ -1287,6 +1287,17 @@ const DiscoverAndMeet = ({ category }) => {
   const [searchedTags, setSearchedTags] = useState([]);
   const [enabledFilters, setEnabledFilters] = useState([]);
 
+  // helper function to check if the enabledFilters contains a particular tag
+  const isTagEnabled = (tag) => {
+    for (let i = 0; i < enabledFilters.length; i++) {
+      if (enabledFilters[i].tag === tag) {
+        return i;
+      }
+    }
+
+    return -1;
+  }
+
   return (
     <div className="page" onScroll={addContent}>
       {/* Search bar and profile/notification buttons */}
@@ -1344,17 +1355,17 @@ const DiscoverAndMeet = ({ category }) => {
           full tag list is only applied when hitting done, which then pushes the info to an active list*/}
           <PopupContent>
             <h2>Project Filters</h2>
-            <div id="filters" className="popup-section">
+            <div id='filters' className='popup-section'>
               <SearchBar
                 dataSets={[{ data: currentTags }]}
                 onSearch={(results) => {
                   setSearchedTags(results[0]);
                 }}
               ></SearchBar>
-              <div id="filter-tabs">
+              <div id='filter-tabs'>
                 {filterPopupTabs.map((tab) => (
                   <a
-                    className="filter-tab"
+                    className='filter-tab'
                     onClick={(e) => {
                       // Remove .selected from all 3 options, add it only to current button
                       let tabs = document.querySelector('#filter-tabs').children;
@@ -1363,7 +1374,7 @@ const DiscoverAndMeet = ({ category }) => {
                       }
                       e.target.classList.add('selected');
                       setCurrentTags(tab.categoryTags);
-                      setSearchedTags(tab.categoryTags);
+                      setSearchedTags({ tags: tab.categoryTags, color: tab.color });
                     }}
                   >
                     {tab.categoryName}
@@ -1371,32 +1382,56 @@ const DiscoverAndMeet = ({ category }) => {
                 ))}
               </div>
               <hr />
-              <div id="filter-tags">
+              <div id='filter-tags'>
                 {searchedTags.length === 0 ? (
                   <p>No tags found. Please make sure you have a tab selected.</p>
                 ) : (
-                  searchedTags.map((tag) => (
+                  searchedTags.tags.map((tag) => (
                     <button
-                      className="tag-button tag-button-green-unselected"
+                      // className={`tag-button tag-button-${searchedTags.color}-unselected`}
+                      className={`tag-button tag-button-${searchedTags.color}-${isTagEnabled(tag) !== -1 ? 'selected' : 'unselected'}`}
                       onClick={(e) => {
-                        setEnabledFilters([...enabledFilters, tag]);
+                        let selecIndex = isTagEnabled(tag);
+
+                        if (selecIndex === -1) {
+                          // Creates an object to store text and category
+                          //setEnabledFilters([...enabledFilters, { tag, color: searchedTags.color }]);
+                          setEnabledFilters([...enabledFilters, { tag, color: searchedTags.color }]);
+                          e.target.classList.replace(
+                            `tag-button-${searchedTags.color}-unselected`, 
+                            `tag-button-${searchedTags.color}-selected`
+                          );
+                        } else {
+                          // Remove tag from list of enabled filters
+                          setEnabledFilters(enabledFilters.toSpliced(selecIndex, 1));
+                          e.target.classList.replace(
+                            `tag-button-${searchedTags.color}-selected`, 
+                            `tag-button-${searchedTags.color}-unselected`
+                          );
+                        }
                       }}
                     >
-                      <i className="fa-solid fa-plus"></i>
+                      <i className='fa-solid fa-plus'></i>
                       {tag}
                     </button>
                   ))
                 )}
               </div>
             </div>
-            <div id="selected" className="popup-section">
+            <div id="selected-section" className="popup-section">
               <h3>Selected</h3>
               <h4>Click to deselect</h4>
               <div id="selected-filters">
                 {enabledFilters.map((tag) => (
-                  <button className="tag-button tag-button-green-selected" onClick={(e) => {}}>
+                  <button 
+                    className={`tag-button tag-button-${tag.color}-selected`}
+                    onClick={(e) => {
+                      // Remove tag from list of enabled filters, re-rendering component
+                      setEnabledFilters(enabledFilters.toSpliced(isTagEnabled(tag.tag), 1));
+                    }}
+                  >
                     <i className="fa-solid fa-x"></i>
-                    {tag}
+                    {tag.tag}
                   </button>
                 ))}
               </div>

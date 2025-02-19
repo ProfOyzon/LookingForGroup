866f0f9aa1000f2a7cdd8456534ad0ba442d3991
xentriom <c04jason@gmail.com>
2025-02-18 22:39:49 -0500

    kept consistency with CRA

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,5 +2,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
+  server: {
+    open: true,
+  },
+  build: {
+    outDir: 'build',
+  },
   plugins: [react()],
 });

a6f7e315d3e28ffb7eb3aa0a322de25d6d5c2c29
MJ-Parson <111760235+LiterallyAFlower@users.noreply.github.com>
2025-05-22 11:07:51 -0400

    adds comments for routes/datasets.js

--- server/routes/datasets.js
+++ server/routes/datasets.js
@@ -11,8 +11,14 @@ router.get('/api/datasets/majors', dataCtrl.getMajors);
 
 //Recieves list of all possible project-types
 router.get('/api/datasets/project-types', dataCtrl.getProjectTypes);
+
+//Recieves list of all tags
 router.get('/api/datasets/tags', dataCtrl.getTags);
+
+//Recieves list of all job-titles
 router.get('/api/datasets/job-titles', dataCtrl.getJobTitles);
+
+//Recieves all socials from datasets
 router.get('/api/datasets/socials', dataCtrl.getSocials);
 
 export default router;

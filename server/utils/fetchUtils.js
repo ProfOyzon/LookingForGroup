6ad615eb9a7a5dae489bc6a218da57f328f97ee6
MJ-Parson <111760235+LiterallyAFlower@users.noreply.github.com>
2025-05-22 15:01:14 -0400

    fetchUtils done, commented, apiURL is lfg.gccis.rit.edu/api...

--- server/utils/fetchUtils.js
+++ server/utils/fetchUtils.js
@@ -43,13 +43,12 @@ catch error
 Base apiURL is:
 */
 
-//const apiURL = '';
+//const apiURL = 'lfg.gccis.rit.edu/api/...';
 
 
 /**
  * Basic GET function for utilities
- * @param {*} apiURL - API to be called
- * @param {*} newData - Data, mapped: eg {key1: 'value1', key2: 'value2'}
+ * @param {*} apiURL - API to be called, if you are using parameters customize the url
  * @returns response - JSONified data or error code.
  */
 function GET(apiURL, newData) {
@@ -73,3 +72,90 @@ function GET(apiURL, newData) {
         })
 }
 
+
+/**
+ * Basic POST function for utilities
+ * @param {*} apiURL - API to be called
+ * @param {*} newData - Data, mapped: eg {key1: 'value1', key2: 'value2'}
+ * @returns response - JSONified data or error code.
+ */
+
+function POST(apiURL, newData) 
+{
+    fetch(apiURL, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(newData)
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log(data); 
+    })
+    .catch(error => {
+        console.error('There was a problem with the fetch operation:', error);
+    });
+}
+
+/**
+ * Basic PUT function for utilities
+ * @param {*} apiURL - API to be called
+ * @param {*} newData - Data, mapped: eg {key1: 'value1', key2: 'value2'}
+ * @returns response - JSONified data or error code.
+ */
+function PUT(apiURL, newData) {
+    fetch(apiURL, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(newData)
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log(data); 
+    })
+    .catch(error => {
+        console.error('There was a problem with the fetch operation:', error);
+    });
+}
+
+
+/**
+ * Basic DELETE function for utilities
+ * @param {*} apiURL - API to be called
+ * @param {*} newData - Data, mapped: eg {key1: 'value1', key2: 'value2'}
+ * @returns response - JSONified data or error code.
+ */
+function DELETE(apiURL) {
+    fetch(apiURL, {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(newData)
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log(data); 
+    })
+    .catch(error => {
+        console.error('There was a problem with the fetch operation:', error);
+    });
+}

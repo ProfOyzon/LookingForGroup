55d76637bd3f4d04be5c1e91a6c1ac87c06afe99
Marigold <111760235+LiterallyAFlower@users.noreply.github.com>
2025-05-22 10:29:20 -0400

    made a generalized backend-dev branch.

--- server/routes/datasets.js
+++ server/routes/datasets.js
@@ -3,8 +3,13 @@ import dataCtrl from '../controllers/datasets.js';
 
 const router = Router();
 
+//Recieves list of all skills
 router.get('/api/datasets/skills', dataCtrl.getSkills);
+
+//Recieves list of all majors
 router.get('/api/datasets/majors', dataCtrl.getMajors);
+
+//Recieves list of all possible project-types
 router.get('/api/datasets/project-types', dataCtrl.getProjectTypes);
 router.get('/api/datasets/tags', dataCtrl.getTags);
 router.get('/api/datasets/job-titles', dataCtrl.getJobTitles);

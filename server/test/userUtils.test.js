a188332457be43ef47fe0b64750efa43832987d3
ForSchoolJackson <jmh4687@g.rit.edu>
2025-05-27 16:09:52 -0400

    added tests search by username and email

--- server/test/userUtils.test.js
+++ server/test/userUtils.test.js
@@ -8,5 +8,16 @@ describe("GET users", () => {
     it('Should return a single user at id=1.', async () => {
         const response = await request('https://lfg.gccis.rit.edu/api').get('/users/1');
         expect(response.statusCode).toBe(200);
-    })
+    });
+    it('Should return single user data by username', async () => {
+        const username = 'userLily';
+        const response = await request('https://lfg.gccis.rit.edu/api').get(`/users/search-username/${username}`);
+        expect(response.statusCode).toBe(200);
+    });
+     it('Should return single user data by email', async () => {
+        const email = 'jmh4687@g.rit.edu'; //returns empty because no account is attatched to it
+        const response = await request('https://lfg.gccis.rit.edu/api').get(`/users/search-email/${email}`);
+        expect(response.statusCode).toBe(200);
+        //console.log(response.body.data);
+    });
 });

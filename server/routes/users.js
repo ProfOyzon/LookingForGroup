5ead3b67d2ff7a29937c5c380e0084ccef1a4d6a
unknown <1>
2025-05-27 10:22:45 -0400

    merge

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import mid from '../middleware/index.js';
 import userCtrl from '../controllers/users.js';
+import pool from '../config/database.js';
 
 const router = Router();
 
@@ -66,6 +67,29 @@ router.get('/api/users/get-username-session', mid.checkLogin, userCtrl.getUserna
  * @returns HTTP response
  */
 router.get('/api/users', userCtrl.getUsers);
+
+router.get('/api/2/users', async (req, res) => {
+  try {
+    let sql = 'SELECT * FROM users WHERE visibility = 1';
+
+    for (const [key, value] of Object.entries(req.query)) {
+      sql += ` AND ${key} = ${value}`;
+    }
+
+    const [users] = await pool.query(sql);
+
+    return res.status(200).json({
+      status: 200,
+      data: users,
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(400).json({
+      status: 400,
+      error: err,
+    });
+  }
+});
 
 /**
  * Gets users by id

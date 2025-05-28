50909ac64ff68eaeff55f5c1ef16cd8b2014e541
unknown <1>
2025-05-28 10:56:09 -0400

    remove

--- server/test/projectsUtils.test.js
+++
@@ -1,9 +0,0 @@
-const request = require('supertest');
-
-describe('API Testing, GET all projects.', () => {
-  it('Should return all projects.', async () => {
-    const response = await request('https://lfg.gccis.rit.edu/api').get('projects');
-    expect(response.statusCode).toBe(200);
-    console.log(response.body);
-  });
-});
--- server/test/userUtils.test.js
+++ server/test/userUtils.test.js
@@ -4,6 +4,7 @@ describe("GET users", () => {
     it('Should return the whole list of users.', async () => {
         const response = await request('https://lfg.gccis.rit.edu/api').get('/users')
         expect(response.statusCode).toBe(200);
+        console.log(response.body);
     });
     it('Should return a single user at id=1.', async () => {
         const response = await request('https://lfg.gccis.rit.edu/api').get('/users/1');

d1f03206ab6ebd8ab86400e3d3cc4fc169e15f80
vwl2 <vwl1415@rit.edu>
2025-05-29 17:29:36 -0400

    credit page and footer improvement
    * moved array of members out of Credits Page (into its own file)
    * changed look of footer to be more prominent
    * adjusted credit page css to look cleaner (on mobile)
    * got rid of confusing identifiers  (ogTeam)
    * added default profile img to be used for credits

--- client/src/components/pages/CreditsPage.tsx
+++ client/src/components/pages/CreditsPage.tsx
@@ -11,137 +11,25 @@ import '../Styles/projects.css';
 import '../Styles/settings.css';
 import '../Styles/pages.css';
 
-
 import { Header } from '../Header';
+import { members } from '../../constants/lfgmembers';
 
+//returns div for html of credits page
 const Credits = () => {
-  // THINGS TO DO:
-  // ADD A LIST OF ALL OF US
-  // MAKE PAGE MOBILE FRIENDLY
-  // MAKE ACCESS TO PAGE MOBILE FRIENDLY
-  // MIGHT WANT TO RETOOL HOW THE TOGGLING OF THE CREDITS PAGE WORKS (MAYBE, IDK)
-  // MORE (PROBABLY)
-
-  // list of og members and their roles
-  const ogMembers: { [key: string]: string } = {
-    'Abraham Furlan': 'Web Developer',
-    'Aeris Peng': 'UI/UX Designer',
-    'Alexander Bok': 'UI/UX Designer',
-    'Andrew Lang': 'Web Developer',
-    'Ben Gomez': 'Web Developer',
-    'Charlie Mendes': 'Web Developer',
-    'Chinmay Gaikwad': 'Web Developer',
-    'Diya Iyer': 'Project Lead',
-    'Elise Hayek': 'UI/UX Designer',
-    'Eloise (Weez) Oyzon': 'Supervisor',
-    'Gerrit Wissink': 'Web Developer',
-    'Halen Ruch': 'UI/UX Designer',
-    'Jennifer Pichardo': 'Web Developer',
-    'Joey Patrum': 'Web Developer',
-    'Joseph Davidson': 'Web Developer',
-    'Joeseph Dunne': 'Web Developer',
-    'Judith (Judy) Derrick': 'Web Developer',
-    'Kaelynn Amodia': 'Web Developer',
-    'Kashaf Ahmed': 'Web Developer',
-    'Matt Ivansek': 'Web Developer',
-    'Samera Vilinskis': 'Web Developer',
-    'Sebastian Arroyo (Bas)': 'UI/UX Designer',
-    'Storm Rivera': 'Web Developer',
-    'Thomas Martinez': 'UI/UX Designer',
-    'Trevor Dunn': 'Web Developer',
-    'Will Steele': 'Producer/Project Lead',
-    'Wilson Xia': 'Web Developer',
-    'Zane London': 'UI/UX Designer',
-  };
-
   return (
     <div className="page" id="my-projects">
       <Header dataSets={[]} onSearch={[]} />
 
       <h1 id="credits-title">Meet The LFG Team</h1>
 
+      {/*runs through an array of all the members and creates a "card" for each one */}
       <div id="credit-members-container">
-        <div className="lfg-contributor">
-          <img className="project-contributor-profile" src="assets/creditProfiles/Weez.png" />
-          <div className="project-contributor-info">
-            <h2 className="team-member-name">Weez</h2>
-            <p className="team-member-role">Supervisor/Project Lead</p>
-          </div>
-        </div>
-
-        <div className="lfg-contributor">
-          <img className="project-contributor-profile" src="assets/creditProfiles/BG.png" />
-          <div className="project-contributor-info">
-            <h2 className="team-member-name">Ben Gomez</h2>
-            <p className="team-member-role">Front-End Developer</p>
-          </div>
-        </div>
-
-        <div className="lfg-contributor">
-          <img className="project-contributor-profile" src="assets/creditProfiles/CB.png" />
-          <div className="project-contributor-info">
-            <h2 className="team-member-name">Corie Beale</h2>
-            <p className="team-member-role">Full Stack Developer</p>
-          </div>
-        </div>
-
-        <div className="lfg-contributor">
-          <img className="project-contributor-profile" src="assets/creditProfiles/CM.png" />
-          <div className="project-contributor-info">
-            <h2 className="team-member-name">Charlie Mendes</h2>
-            <p className="team-member-role">Back-End Developer</p>
-          </div>
-        </div>
-
-        <div className="lfg-contributor">
-          <img className="project-contributor-profile" src="assets/creditProfiles/JF.png" />
-          <div className="project-contributor-info">
-            <h2 className="team-member-name">James Feng</h2>
-            <p className="team-member-role">Back-End Developer</p>
-          </div>
-        </div>
-
-        <div className="lfg-contributor">
-          <img className="project-contributor-profile" src="assets/creditProfiles/JoD.png" />
-          <div className="project-contributor-info">
-            <h2 className="team-member-name">Joseph Dunne</h2>
-            <p className="team-member-role">Front-End Developer</p>
-          </div>
-        </div>
-
-        <div className="lfg-contributor">
-          <img className="project-contributor-profile" src="assets/creditProfiles/JuD.png" />
-          <div className="project-contributor-info">
-            <h2 className="team-member-name">Judy Derrick</h2>
-            <p className="team-member-role">UI/UX Designer</p>
-          </div>
-        </div>
-
-        <div className="lfg-contributor">
-          <img className="project-contributor-profile" src="assets/creditProfiles/KA.png" />
-          <div className="project-contributor-info">
-            <h2 className="team-member-name">Kaelynn Amodia</h2>
-            <p className="team-member-role">UI/UX Designer</p>
-          </div>
-        </div>
-
-        <div className="lfg-contributor">
-          <img className="project-contributor-profile" src="assets/creditProfiles/TD.png" />
-          <div className="project-contributor-info">
-            <h2 className="team-member-name">Trevor Dunn</h2>
-            <p className="team-member-role">UI/UX Designer</p>
-          </div>
-        </div>
-      </div>
-
-      <h1 id="credits-title">Meet The OGs</h1>
-
-      <div id="credit-members-container">
-        {Object.keys(ogMembers).map((name) => (
-          <div className="og-lfg-contributor" key={name}>
+        {members.map(member => (
+          <div className="lfg-contributor" key={member.name}>
+            <img className="project-contributor-profile" src={member.photo} />
             <div className="project-contributor-info">
-              <h2 className="team-member-name">{name}</h2>
-              <p className="team-member-role">{ogMembers[name]}</p>
+              <h2 className="team-member-name">{member.name}</h2>
+              <p className="team-member-role">{member.role}</p>
             </div>
           </div>
         ))}

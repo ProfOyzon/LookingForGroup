99609e3422d0d25c9329224f8df71b50380babb2
MJ-Parson <111760235+LiterallyAFlower@users.noreply.github.com>
2025-06-02 13:15:14 -0400

    user/logins

---
+++ server/test/loginUtils.test.js
@@ -0,0 +1,20 @@
+import utils from "../utils/loginUtils";
+
+/* - - - POSTs - - -*/
+//Login as the user.
+
+
+
+
+
+//create a new user (prompt email). in dev environment, returns token.
+test('Add user information to database, set up for account activtion', async () => {
+    const r = await utils.sendSignup('testaccount','password123','password123','jjp8541@rit.edu','John','Testing');
+
+    console.log('Return code: ',r.status);
+    console.log('Error: ', r.error);
+
+    expect(r.status).toBe(201);
+
+    console.log('proposed token _______________ ',r.data);
+});

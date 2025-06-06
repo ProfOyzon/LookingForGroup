fd3e75d31c57fd500bb32ab5265cec13a02b2706
Thomas <77514919+TB516@users.noreply.github.com>
2025-06-05 22:24:57 -0400

    Fixed eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -7,15 +7,15 @@ import jestPlugin from 'eslint-plugin-jest';
 export default defineConfig([
   {
     files: ['server/test/*.test.js'],
-    plugins: { Jest: jestPlugin },
-    languageOptions: { globals: jest.environments.globals.globals },
-    ...jest.configs['flat/recommended'],
+    plugins: { jest: jestPlugin },
+    languageOptions: { globals: jestPlugin.environments.globals.globals },
+    ...jestPlugin.configs['flat/recommended'],
   },
   {
     files: ['server/**/*.js'],
     plugins: { js },
     extends: ['js/recommended'],
     languageOptions: { globals: globals.node },
-    eslintConfigPrettier,
+    ...eslintConfigPrettier,
   },
 ]);

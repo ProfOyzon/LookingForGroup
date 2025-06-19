c7dee056467c1d236bd23f4f25161b70717d347c
Thomas <77514919+TB516@users.noreply.github.com>
2025-06-18 20:47:05 -0400

    Fix .env import

--- server/src/config/env.js
+++ server/src/config/env.js
@@ -1,7 +1,12 @@
 import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 //load .env file varibles
-dotenv.config({ path: './.env' });
+dotenv.config({ path: path.resolve(__dirname, '../../../.env') });
 
 /**
  * Gets the value of an environment variable.

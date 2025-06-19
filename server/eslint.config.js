37658850a936a3d9b80bf9d0a88ce9baa0dcd5dd
Thomas <77514919+TB516@users.noreply.github.com>
2025-06-19 00:50:44 -0400

    Update eslint.config.js

--- server/eslint.config.js
+++ server/eslint.config.js
@@ -1,36 +1,11 @@
 // @ts-check
 
 import eslint from '@eslint/js';
-import eslintConfigPrettier from 'eslint-config-prettier';
-import jestPlugin from 'eslint-plugin-jest';
 import tseslint from 'typescript-eslint';
+import prettierConfig from 'eslint-config-prettier';
 
 export default tseslint.config(
   eslint.configs.recommended,
-  {
-    plugins: {
-      '@typescript-eslint': tseslint.plugin,
-      jest: jestPlugin,
-    },
-    languageOptions: {
-      parser: tseslint.parser,
-      parserOptions: {
-        projectService: true,
-      },
-    },
-    rules: {
-      '@typescript-eslint/no-floating-promises': 'error',
-    },
-  },
-  {
-    // disable type-aware linting on JS files
-    files: ['**/*.js'],
-    extends: [tseslint.configs.disableTypeChecked],
-  },
-  {
-    // enable jest rules on test files
-    files: ['test/**'],
-    extends: [jestPlugin.configs['flat/recommended']],
-  },
-  eslintConfigPrettier,
+  tseslint.configs.recommended,
+  prettierConfig,
 );

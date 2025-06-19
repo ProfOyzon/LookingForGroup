8344dac2d30c3547315b40fa6c5631d7d9e2544f
Thomas <77514919+TB516@users.noreply.github.com>
2025-06-19 02:14:04 -0400

    Add new swagger docs framework

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,7 @@
 import express, { type Request, type Response } from 'express';
 import morgan from 'morgan';
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
 import envConfig from './config/env.ts';
 
 const app = express();
@@ -8,6 +10,26 @@ const port = envConfig.port;
 app.use(morgan('tiny'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+if (envConfig.env === 'development') {
+  const swaggerDefinition = {
+    openapi: '3.1.1',
+    info: {
+      title: 'Looking For Group API',
+      version: '1.0.0',
+      description:
+        'LFG API Documentation. This will showcase all the endpoints available and how to use them.',
+    },
+    basePath: '/api',
+  };
+
+  const swaggerSpec = swaggerJSDoc({
+    swaggerDefinition,
+    apis: ['../docs/**/*.yaml'],
+  });
+
+  app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+}
 
 app.get('/api', (_req: Request, res: Response) => {
   res.json({ message: 'You Reached The Looking For Group API' });

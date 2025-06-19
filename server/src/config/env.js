fe31394b8891d071fd27a5ae19abb6f313a9585a
Thomas <77514919+TB516@users.noreply.github.com>
2025-06-18 20:51:58 -0400

    Fixed spelling in comment

--- server/src/config/env.js
+++ server/src/config/env.js
@@ -5,7 +5,7 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-//load .env file varibles
+//load .env file variables
 dotenv.config({ path: path.resolve(__dirname, '../../../.env') });
 
 /**

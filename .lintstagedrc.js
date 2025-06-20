cd94ac387a82de915a8d59042684f5a9d5eb27be
Thomas <77514919+TB516@users.noreply.github.com>
2025-06-20 12:49:16 -0400

    Updated lintstaged for npm again

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,7 +1,7 @@
 // Lint staged config file
 // When npx lint-staged is ran by husky on git a git commit
 // Staged files that mach the pattern will have the commands executed on them
-// Once we want to add it back, add this command to the front of the array 'pnpm --filter=server eslint --fix'
+// Once we want to add it back, add this command to the front of the array 'npx --workspace=server eslint --fix'
 export default {
-  "server/**/*": ["pnpm --filter=server prettier --write"],
+  "server/**/*": ["npx --workspace=server prettier --write"],
 };

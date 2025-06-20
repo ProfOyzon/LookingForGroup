cf89363cf36057fafffa81de2151161edfc0fe03
ForSchoolJackson <jmh4687@g.rit.edu>
2025-06-20 14:31:49 -0400

    added api folder in client for api call utils

---
+++ client/src/api/index.ts
@@ -0,0 +1,189 @@
+//response we should get
+export type ApiResponse<_data = any> = {
+    status: number;
+    error?: string | null;
+    data?: _data | null;
+    memetype?: string;
+};
+
+
+//Basic GET function for utilities
+export const GET = async (apiURL: string): Promise<ApiResponse<any>> => {
+    try {
+        const response = await fetch(apiURL, {
+            method: 'GET',
+            credentials: 'include',
+        });
+
+        const contentType = response.headers.get('content-type') || '';
+
+        //check if response is JSON
+        if (contentType.includes('application/json')) {
+
+            //return if json
+            const obj = await response.json();
+
+            if (response.ok) {
+                return { data: obj, status: response.status };
+            } else {
+                console.log(obj.error);
+                return { error: obj.error || 'Network response was not ok', status: response.status };
+            }
+        } else {
+            //handle HTML error pages
+            const html = await response.text();
+            console.error('Expected json but got:', html);
+            return {
+                error: 'Received HTML reponse instead of JSON (Likely broken endpoint)',
+                status: 400,
+            };
+        }
+    } catch (error: any) {
+        console.error('GET error', error);
+        return { error: error.message || 'Unknown error', status: 500 };
+    };
+};
+
+
+
+//Basic POST function
+export const POST = async (apiURL: string, newData: Object): Promise<ApiResponse<any>> => {
+    try {
+        const response = await fetch(apiURL, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            credentials: 'include',
+            body: JSON.stringify(newData),
+        });
+
+        const contentType = response.headers.get('content-type') || '';
+
+        //check if response is JSON
+        if (contentType.includes('application/json')) {
+            //return if json
+            const obj = await response.json();
+
+            if (response.ok) {
+                return { data: obj, status: response.status };
+            } else {
+                console.log(obj.error);
+                return { error: obj.error || 'Network response was not ok', status: response.status };
+            }
+        } else {
+            //handle HTML error pages
+            const html = await response.text();
+            console.error('Expected json but got:', html);
+            return {
+                error: 'Received HTML reponse instead of JSON (Likely broken endpoint)',
+                status: 400,
+            };
+        }
+    } catch (error: any) {
+        console.error('POST error:', error);
+        return { error: error.message || 'Unknown error', status: 400, };
+    };
+};
+
+
+//Basic PUT function
+export const PUT = async (apiURL: string, newData: Object): Promise<ApiResponse<any>> => {
+    try {
+        const response = await fetch(apiURL, {
+            method: 'PUT',
+            headers: { 'Content-Type': 'application/json' },
+            credentials: 'include',
+            body: JSON.stringify(newData),
+        });
+
+        const contentType = response.headers.get('content-type') || '';
+
+        //check if response is JSON
+        if (contentType.includes('application/json')) {
+            //return if json
+            const obj = await response.json();
+
+            if (response.ok) {
+                return { data: obj, status: response.status };
+            } else {
+                console.log(obj.error);
+                return { error: obj.error || 'Network response was not ok', status: response.status };
+            }
+        } else {
+            //handle HTML error pages
+            const html = await response.text();
+            console.error('Expected json but got:', html);
+            return {
+                error: 'Received HTML reponse instead of JSON (Likely broken endpoint)',
+                status: 400,
+            };
+        }
+    } catch (error: any) {
+        console.error('POST error:', error);
+        return { error: error.message || 'Unknown error', status: 400, };
+    };
+};
+
+
+
+//Basic DELETE function
+export const DELETE = async (apiURL: string): Promise<ApiResponse<any>> => {
+    try {
+        const response = await fetch(apiURL, {
+            method: 'DELETE',
+            headers: { 'Content-Type': 'application/json' },
+            credentials: 'include',
+        });
+
+        const contentType = response.headers.get('content-type') || '';
+
+        //check if response is JSON
+        if (contentType.includes('application/json')) {
+            //return if json
+            const obj = await response.json();
+
+            if (response.ok) {
+                return { data: obj, status: response.status };
+            } else {
+                console.log(obj.error);
+                return { error: obj.error || 'Network response was not ok', status: response.status };
+            }
+        } else {
+            //handle HTML error pages
+            const html = await response.text();
+            console.error('Expected json but got:', html);
+            return {
+                error: 'Received HTML reponse instead of JSON (Likely broken endpoint)',
+                status: 400,
+            };
+        }
+    } catch (error: any) {
+        console.error('POST error:', error);
+        return { error: error.message || 'Unknown error', status: 400, };
+    };
+};
+
+
+//jsonify the data
+export function jsonify<_data = any>(
+    data: _data,
+    status: number,
+    error: string | null = null,
+    mimetype = 'application/json'
+) {
+    return {
+        status,
+        mimetype,
+        data,
+        error,
+    };
+};
+
+
+//RESPONSE function
+export const RESPONSE = <_data = any>(
+    status: number,
+    error: string | null = null,
+    data: _data | null = null,
+) => {
+    return jsonify(data, status, error)
+}
---
+++ client/src/api/projects.ts
@@ -0,0 +1,291 @@
+import { GET, POST, PUT, DELETE, RESPONSE, ApiResponse } from './index'
+
+const root = import.meta.env.MODE === 'development'
+    ? 'http://localhost:8081/api'
+    : 'https://lfg.gccis.rit.edu/api'
+
+//or alternatvly use and add API_URL to .env
+// const root = import.meta.env.API_URL;
+
+
+/* PROJECT MANAGMENT */
+
+/**
+ * Creates a new project and adds it to the database. All params default to null.
+ * @param {number} _userId - ID of the user creating the project
+ * @param {string} _title - Name of the project
+ * @param {string} _hook - The short description of the project
+ * @param {string} _desc - The long description of the project
+ * @param {string} _purpose - The purpose selected for this project
+ * @param {string} _status - The status of the project
+ * @param {string} _audience - The project's intended audience
+ * @param {Array<object>} _pTypes - List of project types
+ * @param {Array<object>} _pTags - List of project tags
+ * @param {Array<object>} _jobs - List of roles being recruited for
+ * @param {Array<object>} _members  - List of project members
+ * @param {Array<object>} _socials - List of relevant social media pages
+ * @returns 200 if valid, 400 if not
+ *///might need to change Array<object>
+export const createNewProject = async (
+    _userId: number,
+    _title: string,
+    _hook: string,
+    _desc: string,
+    _purpose: string,
+    _status: string,
+    _audience: string,
+    _pTypes: Array<object>,
+    _pTags: Array<object>,
+    _jobs: Array<object>,
+    _members: Array<object>,
+    _socials: Array<object>,
+): Promise<ApiResponse<any[]>> => {
+    const apiURL = `${root}/projects`;
+
+    const data = {
+        userId: _userId,
+        title: _title,
+        hook: _hook,
+        description: _desc,
+        purpose: _purpose,
+        status: _status,
+        audience: _audience,
+        project_types: _pTypes,
+        tags: _pTags,
+        jobs: _jobs,
+        members: _members,
+        socials: _socials,
+    };
+
+    const response = await POST(apiURL, data);
+    if (response.error) {
+        console.log('Error creating new project:', response.error);
+        return RESPONSE(response.status, response.error);
+    }
+    console.log(`Created project named "${_title}"`);
+    return RESPONSE(200, null, response.data);
+};
+
+/**
+ * Gets all projects in the database
+ * @returns Array of all projects if valid, 400 if not
+ */
+export const getProjects = async (): Promise<ApiResponse<any[]>> => {
+    const apiURL = `${root}/projects`;
+
+    let response = await GET(apiURL);
+
+    if (response.error) {
+        return RESPONSE(response.status, response.error);
+    }
+    return RESPONSE(200, null, response.data);
+};
+
+/**
+ * Retrieves data of a project by its ID
+ * @param {number} ID -  ID of project to retrieve
+ * @returns - A project object if valid, 400 if not
+ */
+export const getByID = async (ID: number): Promise<ApiResponse<any[]>> => {
+    const apiURL = `${root}/projects/${ID}`;
+    let response = await GET(apiURL);
+    if (response.error) {
+        return RESPONSE(response.status, response.error);
+    }
+    return RESPONSE(200, null, response.data);
+};
+
+/**
+ * Updates data of an existing project
+ * @param {number} ID - ID of the project to update
+ * @param {Object} data - Mapped data for update
+ * @returns Response status
+ */
+export const updateProject = async (ID: number, data: object): Promise<ApiResponse<any[]>> => {
+    const apiURL = `${root}/projects/${ID}`;
+    let response = await PUT(apiURL, data);
+    if (response.error) {
+        return RESPONSE(response.status, response.error);
+    }
+    return RESPONSE(200);
+};
+
+
+/**
+ * Deletes an existing project
+ * @param {number} ID - ID of the project to delete
+ * @returns Response status
+ */
+export const deleteProject = async (ID: number): Promise<ApiResponse<any[]>> => {
+    const apiURL = `${root}/projects/${ID}`;
+    let response = await DELETE(apiURL);
+    if (response.error) {
+        return RESPONSE(response.status, response.error);
+    }
+    return RESPONSE(200);
+};
+
+
+/* ASSETS */
+
+/**
+ * Updates the thumbnail image for a project
+ * @param {number} ID - ID of the project to update
+ * @param {File} _image - Image file of new thumbnail
+ * @returns The filename of the thumbnail image if valid, "400" if not
+ */
+export const updateThumbnail = async (ID: number, _image: File): Promise<ApiResponse<any[]>> => {
+    const apiURL = `${root}/projects/${ID}/thumbnail`;
+    const data = { image: _image };
+    let response = await PUT(apiURL, data);
+    if (response.error) {
+        return RESPONSE(response.status, response.error);
+    }
+    return RESPONSE(200, null, response.data);
+};
+
+/**
+ * Gets the pictures used in a project's carousel
+ * @param {number} ID - ID of the target project
+ * @returns Array of image objects if valid, "400" if not
+ */
+export const getPics = async (ID: number): Promise<ApiResponse<any[]>> => {
+    const apiURL = `${root}/projects/${ID}/pictures`;
+    let response = await GET(apiURL);
+    if (response.error) {
+        return RESPONSE(response.status, response.error);
+    }
+    return RESPONSE(200, null, response.data);
+};
+
+/**
+ * Adds a picture to a project's carousel
+ * @param {number} ID - ID of the target project
+ * @param {File} _image - Image file to be added
+ * @param {number} _position - Position of the image in the carousel
+ * @returns Response status
+ */
+export const addPic = async (ID: number, _image: File, _position: number): Promise<ApiResponse<any[]>> => {
+    const apiURL = `${root}/projects/${ID}/pictures`;
+    const data = {
+        image: _image,
+        position: _position,
+    };
+    let response = await POST(apiURL, data);
+    if (response.error) {
+        return RESPONSE(response.status, response.error);
+    }
+    return RESPONSE(200);
+};
+
+/**
+ * Updates position order of a project's carousel pictures
+ * @param {number} ID - ID of the target project
+ * @param {Object} images - Array of objects, which contain the image "id" and new "position"
+ * @returns Response status
+ */
+export const updatePicPositions = async (ID: number, images: Array<{ id: number; position: number }>): Promise<ApiResponse<any[]>> => {
+    const apiURL = `${root}/projects/${ID}/pictures`;
+    let response = await PUT(apiURL, images);
+    if (response.error) {
+        return RESPONSE(response.status, response.error);
+    }
+    return RESPONSE(200);
+};
+
+/**
+ * Deletes a picture in a project
+ * @param {number} ID - ID of the target project
+ * @param {string} image - Filename of the image to delete
+ * @returns Response status
+ */
+export const deletePic = async (ID: number, image: string): Promise<ApiResponse<any[]>> => {
+    //FIX ROUTE FOR DELETING PICTURE
+    //NEEDS TO SPECIFY WHAT PICTURE IS BEING DELETED BY IMAGE NAME
+    //uses encode to evoid special character issues
+    const apiURL = `${root}/projects/${ID}/pictures?image=${encodeURIComponent(image)}`;
+    let response = await DELETE(apiURL);
+    if (response.error) {
+        return RESPONSE(response.status, response.error);
+    }
+    return RESPONSE(200);
+};
+
+
+/* MEMBERS */
+
+/**
+ * Adds a member to a project
+ * @param {number} ID - ID of the target project
+ * @param {number} _userId - ID of the user to add
+ * @param {number} _titleId - ID of the user's role
+ * @param {number} _permission - The user's access level
+ * @returns Response status
+ */
+export const addMember = async (ID: number, _userId: number, _titleId: number, _permission: number): Promise<ApiResponse<any[]>> => {
+    const apiURL = `${root}/projects/${ID}/members`;
+    const data = {
+        userId: _userId,
+        titleId: _titleId,
+        permission: _permission,
+    };
+    let response = await POST(apiURL, data);
+    if (response.error) {
+        return RESPONSE(response.status, response.error);
+    }
+    return RESPONSE(200);
+};
+
+/**
+ * Updates an existing member in a project
+ * @param {number} ID - ID of the target project
+ * @param {number} _userId - ID of the user to update
+ * @param {number} _titleId - ID of the user's role
+ * @param {number} _permission - The user's access level
+ * @returns Response status
+ */
+export const updateMember = async (ID: number, _userId: number, _titleId: number, _permission: number): Promise<ApiResponse<any[]>> => {
+    const apiURL = `${root}/projects/${ID}/members`;
+    const data = {
+        userId: _userId,
+        titleId: _titleId,
+        permission: _permission,
+    };
+    let response = await PUT(apiURL, data);
+    if (response.error) {
+        return RESPONSE(response.status, response.error);
+    }
+    return RESPONSE(200);
+};
+
+/**
+ * Removes a member from a project
+ * @param {number} ID - ID of the target project
+ * @param {number} userId - ID of the target user
+ * @returns Response status
+ */
+export const deleteMember = async (ID: number, userId: number): Promise<ApiResponse<any[]>> => {
+    const apiURL = `${root}/projects/${ID}/members/${userId}`;
+    let response = await DELETE(apiURL);
+    if (response.error) {
+        return RESPONSE(response.status, response.error);
+    }
+    return RESPONSE(200);
+};
+
+
+export default {
+    createNewProject,
+    getProjects,
+    getByID,
+    updateProject,
+    deleteProject,
+    updateThumbnail,
+    getPics,
+    addPic,
+    updatePicPositions,
+    deletePic,
+    addMember,
+    updateMember,
+    deleteMember,
+};

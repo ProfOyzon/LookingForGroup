d61d6ed9580cd749fa684dc15e0c58b1c4742462
Thomas <77514919+TB516@users.noreply.github.com>
2025-06-20 21:21:09 -0400

    Update eslint.config.js

--- server/eslint.config.js
+++ server/eslint.config.js
@@ -4,8 +4,4 @@ import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import prettierConfig from 'eslint-config-prettier';
 
-export default tseslint.config(
-  eslint.configs.recommended,
-  tseslint.configs.recommended,
-  prettierConfig,
-);
+export default tseslint.config(eslint.configs.recommended, tseslint.configs.strict, prettierConfig);

50e64e1d59e53c2dcb1b5d6cacc1ac9bee6e1848
Thomas <77514919+TB516@users.noreply.github.com>
2025-06-22 17:35:15 -0400

    Enabled type aware linting
    Added type aware linting for better error catching when linting

--- server/eslint.config.js
+++ server/eslint.config.js
@@ -4,4 +4,21 @@ import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import prettierConfig from 'eslint-config-prettier';
 
-export default tseslint.config(eslint.configs.recommended, tseslint.configs.strict, prettierConfig);
+export default tseslint.config(
+  eslint.configs.recommended,
+  tseslint.configs.strictTypeChecked,
+  {
+    ignores: ['eslint.config.js'],
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    rules: {
+      eqeqeq: 'error',
+      '@typescript-eslint/restrict-template-expressions': 'warn',
+    },
+  },
+  prettierConfig,
+);

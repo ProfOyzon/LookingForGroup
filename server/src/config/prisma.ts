79417e8b897d81a306a5a34f90ad81c2a9d76469
Thomas <77514919+TB516@users.noreply.github.com>
2025-06-24 15:12:05 -0400

    Update prisma to remove global storage
    Nodemon actually fully kills the node instance and restarts it so the previous changes werent needed

--- server/src/config/prisma.ts
+++ server/src/config/prisma.ts
@@ -1,13 +1,6 @@
 import { PrismaClient } from '../generated/prisma/index.js';
 import envConfig from './env.ts';
 
-// Create a reference to global that has type info for Prisma
-const globalForPrisma = global as unknown as { prisma: PrismaClient | undefined };
-
-// If Prisma doesn't already exist, make a new instance
-const prisma = globalForPrisma.prisma || new PrismaClient({ datasourceUrl: envConfig.databaseUrl });
+const prisma = new PrismaClient({ datasourceUrl: envConfig.databaseUrl });
 
 export default prisma;
-
-// If we aren't in prod, assign the Prisma instance to the global object
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;

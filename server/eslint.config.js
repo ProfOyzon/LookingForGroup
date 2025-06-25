70e8a181c2de9d8b09e3646e648f2fc04fd9af22
Thomas <77514919+TB516@users.noreply.github.com>
2025-06-25 14:37:21 -0400

    Updated eslint to add import rules

--- server/eslint.config.js
+++ server/eslint.config.js
@@ -1,14 +1,17 @@
 // @ts-check
 
-import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
-import prettierConfig from 'eslint-config-prettier';
+import eslint from '@eslint/js';
+import importPlugin from 'eslint-plugin-import';
 import vitest from '@vitest/eslint-plugin';
+import prettierConfig from 'eslint-config-prettier';
 
 export default tseslint.config(
   { ignores: ['eslint.config.js', '**/generated/**'] },
   eslint.configs.recommended,
   tseslint.configs.strictTypeChecked,
+  importPlugin.flatConfigs.recommended,
+  importPlugin.flatConfigs.typescript,
   {
     languageOptions: {
       parserOptions: {
@@ -16,16 +19,19 @@ export default tseslint.config(
         tsconfigRootDir: import.meta.dirname,
       },
     },
-    rules: {
-      eqeqeq: 'error',
-      '@typescript-eslint/restrict-template-expressions': 'warn',
-    },
   },
   {
     files: ['tests/**/*.test.ts'],
     plugins: { vitest },
     rules: {
       ...vitest.configs.recommended.rules,
+    },
+  },
+  {
+    rules: {
+      eqeqeq: 'error',
+      '@typescript-eslint/restrict-template-expressions': 'warn',
+      'import/order': ['error'],
     },
   },
   prettierConfig,

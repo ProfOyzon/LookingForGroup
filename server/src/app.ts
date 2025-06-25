09e614581b6a15d02d75b1e34c9d5726c304d616
Thomas <77514919+TB516@users.noreply.github.com>
2025-06-25 16:01:22 -0400

    Move express app to its own file

---
+++ server/src/app.ts
@@ -0,0 +1,23 @@
+import express, { type Request, type Response } from 'express';
+import morgan from 'morgan';
+import envConfig from '#config/env.ts';
+
+const app = express();
+
+app.use(morgan('tiny'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+if (envConfig.env === 'development') {
+  // Dynamic imports are used here because swagger packages are dev dependencies, which would cause crashes when imported in production.
+  const swaggerSpec = (await import('./config/swagger.ts')).swaggerSpec;
+  const swaggerUi = (await import('swagger-ui-express')).default;
+
+  app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+}
+
+app.get('', (_req: Request, res: Response) => {
+  res.json({ message: 'You Reached The Looking For Group API' });
+});
+
+export default app;

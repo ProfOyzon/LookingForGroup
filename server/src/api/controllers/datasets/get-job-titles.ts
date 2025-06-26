0ce414e8720cf44c9e4bfdd2a5e1d33448f6082d
Thomas <77514919+TB516@users.noreply.github.com>
2025-06-26 14:34:11 -0400

    Created api directory for any code route/controller code

---
+++ server/src/api/controllers/datasets/get-job-titles.ts
@@ -0,0 +1,6 @@
+import type { Request, Response } from 'express';
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const getJobTitlesController = async (request: Request, response: Response): Promise<void> => {};
+
+export default getJobTitlesController;

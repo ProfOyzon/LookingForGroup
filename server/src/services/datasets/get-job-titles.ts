eed0ab1efbd45e02c9d723aeaab11fe2537ab6fb
Thomas <77514919+TB516@users.noreply.github.com>
2025-06-26 16:25:28 -0400

    Added logic to getJobTitlesService

--- server/src/services/datasets/get-job-titles.ts
+++ server/src/services/datasets/get-job-titles.ts
@@ -1,3 +1,18 @@
-const getJobTitlesService = () => {};
+import prisma from '#config/prisma.ts';
+import type { ServiceError } from '#types/service-error.ts';
+
+const getJobTitlesService = async (): Promise<string[] | ServiceError> => {
+  try {
+    const result = await prisma.job_titles.findMany({ select: { label: true } });
+
+    return result.map((title) => {
+      return title.label;
+    });
+  } catch (e) {
+    console.error(`Error in getJobTitlesService: ${JSON.stringify(e)}`);
+
+    return 'INTERNAL_ERROR';
+  }
+};
 
 export default getJobTitlesService;

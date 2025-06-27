425a8d1d4c48a0cf4893af3482f3eac6cfdced22
Thomas <77514919+TB516@users.noreply.github.com>
2025-06-27 13:53:48 -0400

    Update prisma schema

--- server/src/services/datasets/get-job-titles.ts
+++ server/src/services/datasets/get-job-titles.ts
@@ -1,15 +1,12 @@
 import prisma from '#config/prisma.ts';
+import type { JobTitles } from '#prisma-models/index.js';
 import type { ServiceErrorSubtype } from '#services/service-error.ts';
 
 type GetJobTitlesServiceError = ServiceErrorSubtype<'INTERNAL_ERROR'>;
 
-const getJobTitlesService = async (): Promise<string[] | GetJobTitlesServiceError> => {
+const getJobTitlesService = async (): Promise<JobTitles[] | GetJobTitlesServiceError> => {
   try {
-    const result = await prisma.job_titles.findMany({ select: { label: true } });
-
-    return result.map((title) => {
-      return title.label;
-    });
+    return await prisma.jobTitles.findMany();
   } catch (e) {
     console.error(`Error in getJobTitlesService: ${JSON.stringify(e)}`);
 

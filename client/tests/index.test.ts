08665afc91a249d616f0d3575dcd058385d26fe3
Thomas <77514919+TB516@users.noreply.github.com>
2025-07-01 14:45:22 -0400

    Updated e2e tests in client to start server automatically

--- client/tests/index.test.ts
+++ client/tests/index.test.ts
@@ -1,29 +1,34 @@
+import { afterAll, beforeAll, expect, test } from "vitest";
+import http from "http";
+import app from "@looking-for-group/server";
 import util from "../src/api/index.js";
-import { expect, test } from "vitest";
 
-/**
+let server: http.Server;
+let port: number;
+
+beforeAll(async () => {
+  server = http.createServer(app);
+
+  await new Promise<void>((resolve) => server.listen(0, resolve));
+
+  port = (server.address() as { port: number }).port;
+});
+
+afterAll(async () => {
+  await new Promise((resolve) => server.close(resolve));
+});
+
+/*
  * Test basic gets
  */
-test("lfg: Test gets users", async () => {
-  const apiURL = "https://lfg.gccis.rit.edu/api/users";
-  const result = await util.GET(apiURL);
-  expect(result.status).toBe(200);
-});
-
 test("local: Test gets users", async () => {
-  const apiURL = "http://localhost:8081/api/users";
-  const result = await util.GET(apiURL);
-  expect(result.status).toBe(200);
-});
-
-test("lfg: Test gets projects", async () => {
-  const apiURL = "https://lfg.gccis.rit.edu/api/projects";
+  const apiURL = `http://localhost:${port}/users`;
   const result = await util.GET(apiURL);
   expect(result.status).toBe(200);
 });
 
 test("local: Test gets projects", async () => {
-  const apiURL = "http://localhost:8081/api/projects";
+  const apiURL = `http://localhost:${port}/projects`;
   const result = await util.GET(apiURL);
   expect(result.status).toBe(200);
 });

795e964e401af0daedc062e6b7b60d9fc114f479
Thomas <77514919+TB516@users.noreply.github.com>
2025-07-02 13:06:52 -0400

    Requires login middleware
    Still needs the headers replaced, but outside of that it should be all good

---
+++ server/src/api/middleware/requiresLogin.ts
@@ -0,0 +1,41 @@
+import type { NextFunction, Request, Response } from 'express';
+import envConfig from '#config/env.ts';
+import uidExistsService from '#services/users/uid-exists.ts';
+
+const requiresLogin = async (request: Request, response: Response, next: NextFunction) => {
+  if (envConfig.env === 'development') {
+    /// Add UID for development, missing correct header
+    request.headers['x-'] = '000000001';
+
+    next();
+    return;
+  }
+
+  /// Add correct header when we have it
+  const uidHeader = request.headers[''] as string | undefined;
+
+  if (uidHeader === undefined) {
+    response.status(401).json({ message: 'You must log in to access this resource' });
+    return;
+  }
+
+  /// If the server is set up properly any defined UIDs should be valid, but better safe than sorry
+  try {
+    const uid = Number.parseInt(uidHeader, 10);
+
+    const exists = await uidExistsService(uid);
+
+    if (!exists) {
+      response.json({ message: 'You must log in to access this resource' });
+      return;
+    }
+
+    next();
+    return;
+  } catch {
+    response.json({ message: 'You must log in to access this resource' });
+    return;
+  }
+};
+
+export default requiresLogin;

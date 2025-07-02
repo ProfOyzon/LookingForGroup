6259705baacea8c19521f68aed7a6efe547affa6
JPatrum <jdp1701@g.rit.edu>
2025-07-02 10:36:17 -0400

    Prepped project routes

--- server/src/api/routes/projects.ts
+++ server/src/api/routes/projects.ts
@@ -7,29 +7,39 @@ const router = Router();
 router.get('/', getProjects);
 
 //Create a new project
+router.post('/' /* createProject */);
 
 //Get a specific project
-//router.get('/:id', getProjectById);
+router.get('/:id' /* getProjectById */);
 
 //Edits a project through a specific id
+router.put('/:id' /* updateProject */);
 
 //Deletes project through a specific id
+router.delete('/:id' /* deleteProject */);
 
 //Edits a project thumbnail through a specific id
+router.put(':/id/thumbnail' /* updateThumbnail */);
 
 //Receives picture from project through id
-//router.get('/:id/pictures', getPictures);
+router.get('/:id/pictures' /* getPictures */);
 
 //Creates a new picture for a project
+router.post('/:id/pictures' /* addPicture */);
 
 //Changes a picture for a project
+router.put('/:id/pictures' /* changePicture */);
 
 //Removes picture from a project
+router.delete('/:id/pictures' /* deletePicture */);
 
 //Adds member to a specific project through id
+router.post('/:id/members' /* addMember */);
 
 //Edits a member of a specific project through id
+router.put('/:id/members' /* updateMember */);
 
 //Removes a member from a specific project through project and user ID
+router.delete('/:id/members' /* deleteMember */);
 
 export default router;

bcd9e942f3dd97aa8e59ceeb78e5029dc2969c75
Thomas <77514919+TB516@users.noreply.github.com>
2025-07-03 15:35:43 -0400

    Add conflict to enum

--- server/src/services/service-error.ts
+++ server/src/services/service-error.ts
@@ -1,5 +1,5 @@
 // Master list of all possible error messages that a service can have, expand as needed
-type ServiceError = 'INTERNAL_ERROR' | 'NOT_FOUND';
+type ServiceError = 'INTERNAL_ERROR' | 'NOT_FOUND' | 'CONFLICT';
 
 // This utility allows us to use ServiceError as a list of all possible output values
 // while making subtypes that only have some of the values which is then enforced by TypeScript

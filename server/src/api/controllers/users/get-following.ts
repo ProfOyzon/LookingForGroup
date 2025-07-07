0e4ff4c9e4519816163438ba2243c0c943d1e11e
1 <1@>
2025-07-07 10:38:11 -0400

    following

--- server/src/api/controllers/users/get-following.ts
+++ server/src/api/controllers/users/get-following.ts
@@ -1,6 +1,27 @@
 import type { Request, Response } from 'express';
+import prisma from '#config/prisma.ts';
 
 //get the user by the username
-export const getUserFollowing = (_req: Request, res: Response): void => {
-  res.status(200);
+export const getUserFollowing = async (req: Request, res: Response): Promise<void> => {
+  const { userIdStr } = req.params;
+
+  const userId = parseInt(userIdStr);
+
+  try {
+    //should be unique
+    const followings = await prisma.userFollowings.findMany({
+      where: { userId },
+      select: {
+        userId: true,
+        followingId: true,
+        followedAt: true,
+      },
+    });
+
+    res.status(200).json(followings);
+  } catch (e) {
+    console.error(`Error in getUserFollowing: ${JSON.stringify(e)}`);
+    res.status(500).json({ message: 'Internal Server Error' });
+    return;
+  }
 };

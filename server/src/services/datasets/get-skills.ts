09c924d4b62e4f5bbceaa5811b9bf0ddb7f8e5b4
Thomas <77514919+TB516@users.noreply.github.com>
2025-07-08 12:19:48 -0400

    Update get-skills.ts

--- server/src/services/datasets/get-skills.ts
+++ server/src/services/datasets/get-skills.ts
@@ -1,17 +1,17 @@
 import prisma from '#config/prisma.ts';
-import type { skills } from '#prisma-models/index.js';
+import type { Skills } from '#prisma-models/index.js';
 import type { ServiceErrorSubset } from '#services/service-error.ts';
 
 type GetSkillsServiceError = ServiceErrorSubset<'INTERNAL_ERROR'>;
 
-const getSkillsService = async (): Promise<skills[] | GetSkillsServiceError> => {
+const getSkillsService = async (): Promise<Skills[] | GetSkillsServiceError> => {
   try {
     return await prisma.skills.findMany({
       where: {
         type: 'Developer',
       },
       select: {
-        skill_id: true,
+        skillId: true,
         label: true,
         type: true,
       },

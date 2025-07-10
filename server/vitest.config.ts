adaac4e2bfa017ca69d35e54130473f02ef9c823
TB516 <tberrios9800@gmail.com>
2025-07-10 11:54:10 -0400

    Load .env in server vitest

--- server/vitest.config.ts
+++ server/vitest.config.ts
@@ -1,7 +1,11 @@
 import { defineProject } from 'vitest/config';
+import dotenv from 'dotenv';
+
+dotenv.config({ path: '../.env' });
 
 export default defineProject({
   test: {
     environment: 'node',
+    env: { ...process.env },
   },
 });

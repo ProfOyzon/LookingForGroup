eb7984064feb96f53c7ec663557071d02bf84e73
ForSchoolJackson <jmh4687@g.rit.edu>
2025-07-10 13:27:43 -0400

    added 404 error to get projects by is

--- server/src/api/controllers/projects/get-proj-pictures.ts
+++ server/src/api/controllers/projects/get-proj-pictures.ts
@@ -29,6 +29,17 @@ const getProjectPicturesController = async (_req: Request, res: Response): Promi
     return;
   }
 
+  if (result === 'NOT_FOUND') {
+    const resBody: ApiResponse = {
+      status: 404,
+      error: 'Project not found',
+      data: null,
+      memetype: 'application/json',
+    };
+    res.status(404).json(resBody);
+    return;
+  }
+
   const resBody: ApiResponse<typeof result> = {
     status: 200,
     error: null,

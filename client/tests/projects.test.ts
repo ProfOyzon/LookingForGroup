f11aff0f9b39269af0494ec063f60c0076c95012
Thomas <77514919+TB516@users.noreply.github.com>
2025-07-11 15:30:40 -0400

    Updated projects test to use new framework

--- client/tests/projects.test.ts
+++ client/tests/projects.test.ts
@@ -1,22 +1,10 @@
 import { afterAll, beforeAll, expect, test } from "vitest";
-import http from "http";
-import app from "@looking-for-group/server";
 import util from "../src/api/projects.ts";
+import { startTestServer, stopTestServer } from "./server-management.ts";
 
-let server: http.Server;
-let port: number;
+beforeAll(startTestServer);
 
-beforeAll(async () => {
-  server = http.createServer(app);
-
-  await new Promise<void>((resolve) => server.listen(0, resolve));
-
-  port = (server.address() as { port: number }).port;
-});
-
-afterAll(async () => {
-  await new Promise((resolve) => server.close(resolve));
-});
+afterAll(stopTestServer);
 
 //\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\
 // USERS
@@ -25,32 +13,32 @@ afterAll(async () => {
 /**
  * getProjects
  */
-test('Test getProjects, local. 1: Test status. 2: Test data.', async () => {
-    const result = await util.getProjects();
-    console.log(result);
-    expect( result.status ).toBe(200);
-    expect( result.data ).toBeDefined();
+test("Test getProjects, local. 1: Test status. 2: Test data.", async () => {
+  const result = await util.getProjects();
+  console.log(result);
+  expect(result.status).toBe(200);
+  expect(result.data).toBeDefined();
 });
 
 /**
  * getByID
  */
-test('Test getByID, local. 1: Test status. 2: Test data.', async () => {
-    const result = await util.getByID(1);
-    console.log(result);
-    expect( result.status ).toBe(200);
-    expect( result.data ).toBeDefined();
+test("Test getByID, local. 1: Test status. 2: Test data.", async () => {
+  const result = await util.getByID(1);
+  console.log(result);
+  expect(result.status).toBe(200);
+  expect(result.data).toBeDefined();
 });
 
 /**
  * getPics
  */
-test('Test getPics, local. 1: Test status. 2: Test data.', async () => {
-    const result = await util.getPics(1);
-    console.log(result);
-    expect( result.status ).toBe(200);
-    expect( result.data ).toBeDefined();
-})
+test("Test getPics, local. 1: Test status. 2: Test data.", async () => {
+  const result = await util.getPics(1);
+  console.log(result);
+  expect(result.status).toBe(200);
+  expect(result.data).toBeDefined();
+});
 
 //\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\
 // POSTs

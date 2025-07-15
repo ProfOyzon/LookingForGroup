6a4aca0e8b660393290b802c780abd53e0380179
Thomas <tberrios9800@gmail.com>
2025-07-14 22:46:54 -0400

    Added constants for other headers

--- server/src/config/constants.ts
+++ server/src/config/constants.ts
@@ -1,3 +1,7 @@
 const isLoggedInHeader = 'logged-in';
+const uidHeader = 'uid';
+const firstNameHeader = 'givenName';
+const lastNameHeader = 'sn';
+const emailHeader = 'mail';
 
-export { isLoggedInHeader };
+export { isLoggedInHeader, uidHeader, firstNameHeader, lastNameHeader, emailHeader };

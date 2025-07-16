73463db46361b7d6ed51a147a971ea166dea7f0c
Thomas <tberrios9800@gmail.com>
2025-07-15 22:48:40 -0400

    Requires login uses new const names

--- server/src/api/middleware/requires-login.ts
+++ server/src/api/middleware/requires-login.ts
@@ -1,17 +1,17 @@
 import type { NextFunction, Request, Response } from 'express';
-import { isLoggedInHeader, uidHeader } from '#config/constants.ts';
+import { isLoggedInHeaderKey, uidHeaderKey } from '#config/constants.ts';
 import envConfig from '#config/env.ts';
 
 const requiresLogin = (request: Request, response: Response, next: NextFunction) => {
   if (envConfig.env === 'development' || envConfig.env === 'test') {
     /// Add UID for development, missing correct header
-    request.headers[uidHeader] = '000000001';
+    request.headers[uidHeaderKey] = '000000001';
 
     next();
     return;
   }
 
-  if (request.headers[isLoggedInHeader] === 'true') {
+  if (request.headers[isLoggedInHeaderKey] === 'true') {
     next();
     return;
   }

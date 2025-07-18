0a94b32138687aa25542155fcdc1fecd4e73ecd8
Thomas <77514919+TB516@users.noreply.github.com>
2025-07-18 16:17:53 -0400

    Deleted delete project service and updated controller to return 501

--- server/src/api/controllers/projects/delete-project.ts
+++ server/src/api/controllers/projects/delete-project.ts
@@ -1,42 +1,43 @@
 import type { ApiResponse } from '@looking-for-group/shared';
 import type { Request, Response } from 'express';
-import type { Prisma } from '#prisma-models/index.js';
-import deleteService from '#services/projects/delete-proj.ts';
 
-const deleteProjectController = async (req: Request, res: Response) => {
-  const { id } = req.params;
-
-  const result = await deleteService(id);
-
-  if (result === 'NOT_FOUND') {
-    const resBody: ApiResponse = {
-      status: 404,
-      error: 'Project not found',
-      data: null,
-      memetype: 'application/json',
-    };
-    res.status(404).json(resBody);
-    return;
-  }
-
-  if (result === 'INTERNAL_ERROR') {
-    const resBody: ApiResponse = {
-      status: 500,
-      error: 'Internal Server Error',
-      data: null,
-      memetype: 'application/json',
-    };
-    res.status(500).json(resBody);
-    return;
-  }
-
-  const resBody: ApiResponse = {
-    status: 200,
-    error: null,
-    data: result,
+const deleteProjectController = (req: Request, res: Response) => {
+  res.status(501).json({
+    status: 501,
+    error: 'NOT IMPLEMENTED',
+    data: null,
     memetype: 'application/json',
-  };
-  res.status(200).json(resBody);
+  } as ApiResponse);
+
+  // const { id } = req.params;
+  // const result = await deleteService(id);
+  // if (result === 'NOT_FOUND') {
+  //   const resBody: ApiResponse = {
+  //     status: 404,
+  //     error: 'Project not found',
+  //     data: null,
+  //     memetype: 'application/json',
+  //   };
+  //   res.status(404).json(resBody);
+  //   return;
+  // }
+  // if (result === 'INTERNAL_ERROR') {
+  //   const resBody: ApiResponse = {
+  //     status: 500,
+  //     error: 'Internal Server Error',
+  //     data: null,
+  //     memetype: 'application/json',
+  //   };
+  //   res.status(500).json(resBody);
+  //   return;
+  // }
+  // const resBody: ApiResponse = {
+  //   status: 200,
+  //   error: null,
+  //   data: result,
+  //   memetype: 'application/json',
+  // };
+  // res.status(200).json(resBody);
 };
 
 export default deleteProjectController;

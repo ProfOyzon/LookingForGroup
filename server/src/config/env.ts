abc0301a9ca5d4f34866d27f5fa7711163ee294d
Thomas <77514919+TB516@users.noreply.github.com>
2025-07-18 16:24:35 -0400

    Removed old requirements for env variables (doesnt align with new server structure as if these are missing the server will still run, just return 500s)

--- server/src/config/env.ts
+++ server/src/config/env.ts
@@ -1,40 +1,7 @@
-/**
- * Gets the value of an environment variable.
- * @param {string} key Name of the environment variable to retrieve
- * @throws {Error} If the environment variable is not defined
- * @returns {string} Value of the environment variable
- */
-const getEnvVariable = (key: string): string => {
-  const value = process.env[key];
-
-  if (!value) {
-    throw new Error(`Environment variable ${key} is not defined!`);
-  }
-
-  return value;
-};
-
-/**
- * Gets the value of an environment variable and converts it to a number.
- * @param {string} key Name of the environment variable to retrieve
- * @throws {Error} If the environment variable is not defined or is not a valid number
- * @returns {number} Value of the environment variable as a number
- */
-const getNumEnvVariable = (key: string): number => {
-  const value = getEnvVariable(key);
-  const numValue = Number.parseInt(value, 10);
-
-  if (Number.isNaN(numValue)) {
-    throw new Error(`Environment variable ${key} is not a valid number!`);
-  }
-
-  return numValue;
-};
-
 const envConfig = Object.freeze({
-  env: getEnvVariable('NODE_ENV') as 'production' | 'development' | 'test',
-  databaseUrl: `mysql://${getEnvVariable('DB_USER')}:${getEnvVariable('DB_PASS')}@${getEnvVariable('DB_HOST')}:${getNumEnvVariable('DB_PORT')}/${getEnvVariable('DB_NAME')}`,
-  port: getNumEnvVariable('PORT'),
+  env: process.env.NODE_ENV ?? ('production' as 'production' | 'development' | 'test'),
+  databaseUrl: `mysql://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
+  port: process.env.PORT ?? 3000,
 });
 
 export default envConfig;
